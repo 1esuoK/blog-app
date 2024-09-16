18e4bf66a16a83560ce300f9c89c3445f21974f8
1esuoK <imtrituongg@gmail.com>
2024-09-16 15:51:39 +0700

    create post functionality

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { UserContextProvider } from './UserContext';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
         }/>
         <Route path="/register" element={
             <RegisterPage/>
+        }/>
+        <Route path="/create" element={
+          <CreatePost/>
         }/>
         </Route>
       </Routes>
